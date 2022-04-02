36bd4f450bf206241cde086cc0fd3ebcecfd55fe
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-04-01 21:36:38 -0400

    Added place holder objects for questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,100 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'xxx',
+        message:'Enter your xxx',
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log('Sorry, please enter xxx to continue');
+                return false
+            }
+        }
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
